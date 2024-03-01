8dc392d0cd863ffebf31ce8f4e88a6ea973d3917
kanatnikovmaxim@yandex.ru <kanatnikovmaxim@yandex.ru>
2024-03-01 15:29:55 +0300

    добавил первую лабу по алгоритмам

---
+++ Каплиева_лаба1_STL/Lab1/PrintEdition.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+
+class PrintEdition
+{
+protected:
+
+	std::string name{};
+	size_t pages{};
+	std::string publishing_house{};
+
+public:
+
+	PrintEdition(std::string name, size_t pages, std::string ph) :
+		name(name), pages(pages), publishing_house(ph) {}
+
+	virtual void print(std::ostream& out = std::cout) = 0;
+
+};
+
+class Book : public PrintEdition
+{
+private:
+
+	std::string author{};
+	std::string genre{};
+	std::string binding_type{};
+
+public:
+
+	Book(std::string name, size_t pages, std::string ph,
+		std::string author, std::string genre, std::string bt) :
+		PrintEdition(name, pages, ph), author(author),
+		genre(genre), binding_type(bt) {}
+
+	void print(std::ostream& out = std::cout);
+
+};
+
+class Magazine : public PrintEdition
+{
+private:
+
+	size_t number{};
+	size_t year{};
+	std::string web_site{};
+
+public:
+
+	Magazine(std::string name, size_t pages, std::string ph,
+		size_t number, size_t year, std::string web_site) :
+		PrintEdition(name, pages, ph), number(number),
+		year(year), web_site(web_site) {}
+
+	void print(std::ostream& out = std::cout);
+
+	std::string get_web_site()
+	{
+		return web_site;
+	}
+};
