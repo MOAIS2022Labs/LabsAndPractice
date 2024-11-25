c01b32d73714373cb4d2d54f30d743130db56ec0
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2024-11-25 10:50:41 +0300

    OS lab1 226

---
+++ Голукович/3 курс 1 семестр/Операционные системы/Lab 1/Lab1_OS_226/Lab1_OS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Lab1_OS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab1_OS")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("128b9fb7-7769-4f44-ad46-643e5c729f11")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Голукович/3 курс 1 семестр/Операционные системы/Lab 1/Lab1_OS_226/Lab1_OS/ThreadSafeBuffer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace OS_Lab1.Model
+{
+    public class ThreadSafeBuffer //сама модель буффер
+    {
+        //очередь, хранит в себе (int, int) - номер потока и значение (индекс)
+        private Queue<(int, int)> _threadSafeQueue = new Queue<(int, int)>();
+        //максимальный размер буффера
+        private int _maxBufferSize;
+        //семафор пустоты
+        private Semaphore _emptySemaphore;
+        //семафор полноты
+        private Semaphore _maxSizeSemaphore;
+        //мютекс для критической секции
+        private Mutex _mutex = new Mutex();
+        //тот самый action, задаваемый из формы, печатающий что-либо на форму
+        private Action<string> _writeAction;
+
+        //конструктор, передаем размер и action
+        public ThreadSafeBuffer(int maxBufferSize, Action<string> writeAction)
+        {
+            _maxBufferSize = maxBufferSize;
+            _writeAction = writeAction;
+            _emptySemaphore = new Semaphore(0, _maxBufferSize);
+            _maxSizeSemaphore = new Semaphore(_maxBufferSize, _maxBufferSize);
+    }
+
+        //собственно метод push (кладем (int, int), что это - писал выше)
+        public void Push((int, int) value)
+        {
+            //ждем, пока семафор полноты нас не уведомит, что в буффере есть место
+            _maxSizeSemaphore.WaitOne();
+            //заходим в крит секцию
+            _mutex.WaitOne();
+            //кладем
+            _threadSafeQueue.Enqueue(value);
+            //печатаем в форму
+            _writeAction(value + " push");
+            //выходим из крит секции
+            _mutex.ReleaseMutex();
+            //уведомляем семафор пустоты, что буффер теперь не пуст
+            _emptySemaphore.Release();
+        }
+
+        //собственно метод pop
+        public void Pop()
+        {
+            //ждем, пока семафор пустоты нас не уведомит, что в буффере есть место
+            _emptySemaphore.WaitOne();
+            //заходим в крит секцию
+            _mutex.WaitOne();
+            //забираем
+            (int, int) value = _threadSafeQueue.Dequeue();
+            //печатаем в форму
+            _writeAction(value + " pop");
+            //уведомляем семафор полноты, что буффер теперь не пуст
+            _maxSizeSemaphore.Release();
+            //выходим из крит секции
+            _mutex.ReleaseMutex();
+        }
+
+        //очистка буффера
+        public void Clear()
+        {
+            _threadSafeQueue.Clear();
+            _maxBufferSize = 0;
+        }
+    }
+}
